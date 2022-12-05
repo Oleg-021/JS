3f1b898962f53706bd07e3ed4b75c02588fcab1d
Oleg(windows) <oleg21@yandex.ru>
2022-12-05 18:42:24 +0300

    commit

--- stage 3/js/3.2.2.js
+++ stage 3/js/3.2.2.js
@@ -16,6 +16,25 @@ a,b'
 arraysToCsv([[1, 2], ['a,b', 'c,d']]) // '1,2
 "a,b","c,d"' */
 
-function arrayToCsv(data) {
-
+function arraysToCsv(data) {
+    return data.map(nestedArr => nestedArr.map(item => {
+        if (typeof item === "string" || typeof item === "number") {
+            if (item.toString().includes(",") || item.toString().includes(";") ||
+                item.toString().includes("\"") || item.toString().includes("'" ||
+                    item.toString().includes("`"))) {
+                return `"${item.replace(/\"/g, '""')}"`;
+            } else
+                return item;
+        } else
+            throw new Error("Unexpected value");
+    }).join(",")).join("\n");
 }
+
+
+
+
+
+console.log(arraysToCsv([[1, 2], ['a', 'b']]));
+console.log(arraysToCsv([[1, 2], ['`a,b`', 'c,d', "jkh"]]));
+
+
